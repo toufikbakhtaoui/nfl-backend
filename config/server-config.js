bb73ac908789425a4f68239d4d52dc4eb277f752
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2019-12-22 06:25:01 +0100

    add swagger configuration

--- config/server-config.js
+++ config/server-config.js
@@ -1,6 +1,9 @@
+const swagger = require('./swagger-config')
 const fastify = require('fastify')({
     logger: true
 })
+
+fastify.register(require('fastify-swagger'), swagger.options)   
 
 fastify.get('/', async(request, reply) => {
     return 'Welcome to the nfl'
@@ -9,6 +12,7 @@ fastify.get('/', async(request, reply) => {
 const server = async () => {
     try {
         await fastify.listen(3000)
+        fastify.swagger()
         fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (error) {
         fastify.log.error(err)
