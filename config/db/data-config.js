8994f30bfb7a0acd7450df13e5df6293ecc993ba
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2020-01-01 02:58:17 +0100

    extract teams config from data config

--- config/db/data-config.js
+++ config/db/data-config.js
@@ -1,17 +1,3 @@
-const teamModel = require('../../api/teams/team')
-
-const initTeams = async () => {
-    const teams = await teamModel.find()
-    if (teams && teams.length === 32) {
-        return
-    }
-    let team = null
-    const teamList = require('./data/teams.json')
-    teamList.forEach(element => {
-        team = new teamModel(element)
-        teams.push(team)
-    })
-    await teamModel.insertMany(teams)
-}
+const initTeams = require('./teams-config')
 
 module.exports = initTeams
---
+++ config/db/teams-config.js
@@ -0,0 +1,17 @@
+const teamModel = require('../../api/teams/team')
+
+const initTeams = async () => {
+    const teams = await teamModel.find()
+    if (teams && teams.length === 32) {
+        return
+    }
+    let team = null
+    const teamList = require('./data/teams.json.js')
+    teamList.forEach(element => {
+        team = new teamModel(element)
+        teams.push(team)
+    })
+    await teamModel.insertMany(teams)
+}
+
+module.exports = initTeams
